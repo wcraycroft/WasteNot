60d35fcf4bdb55453fabde161dfbc941523817fb
wcraycroft <wrcraycroft@gmail.com>
2019-05-14 00:54:42 -0700

    Cleaned up comments.

--- app/src/main/java/cs134/miracosta/wastenot/Model/DBHelper.java
+++ app/src/main/java/cs134/miracosta/wastenot/Model/DBHelper.java
@@ -1,27 +1,6 @@
-// TODO: WIP just using as template
-// Firebase code
+// TODO: For reference only. Delete when done, use FirebaseDBHelper.
 
-// Write to DB
-// FirebaseDatabase database = FirebaseDatabase.getInstance();
-// DatabaseReference myRef = database.getReference("TestMessage");
-// myRef.setValue("Hello, World!");
 
-// Read from the database using event listener
-//myRef.addValueEventListener(new ValueEventListener() {
-//    @Override
-//    public void onDataChange(DataSnapshot dataSnapshot) {
-//        // This method is called once with the initial value and again
-//        // whenever data at this location is updated.
-//        String value = dataSnapshot.getValue(String.class);
-//        Log.d(TAG, "Value is: " + value);
-//    }
-//
-//    @Override
-//    public void onCancelled(DatabaseError error) {
-//        // Failed to read value
-//        Log.w(TAG, "Failed to read value.", error.toException());
-//    }
-//});
 
 package cs134.miracosta.wastenot.Model;
 
